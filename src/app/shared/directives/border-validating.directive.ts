508b53781fe62af8e535a1b162e54c790d4d6511
stepa-kuropatkin <igortrotsuk@gmail.com>
2021-11-26 12:01:38 +0300

    style: modal styles

--- src/app/shared/directives/border-validating.directive.ts
+++ src/app/shared/directives/border-validating.directive.ts
@@ -4,17 +4,16 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[appBorderValidating]',
 })
 export class BorderValidatingDirective {
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) {
+    this.el.nativeElement.style.border = 'none';
+    this.el.nativeElement.style.borderRadius = '3px';
+  }
 
   @Input() set appBorderValidating(isValid: boolean) {
-    if (!this.el.nativeElement.disabled) {
-      if (!isValid) {
-        this.el.nativeElement.style.border = '2px solid green';
-        this.el.nativeElement.style.borderRadius = '5px';
-      } else if (isValid) {
-        this.el.nativeElement.style.border = '2px solid red';
-        this.el.nativeElement.style.borderRadius = '5px';
-      }
+    if (!isValid) {
+      this.el.nativeElement.style.outline = '2px solid green';
+    } else if (isValid) {
+      this.el.nativeElement.style.outline = '2px solid red';
     }
   }
 }
