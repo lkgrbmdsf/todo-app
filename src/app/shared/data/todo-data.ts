b4bc27d01f0f2695211901ad840c94fd7a8d1607
stepa-kuropatkin <igortrotsuk@gmail.com>
2021-12-03 00:12:18 +0300

    feat: delete additional props

--- src/app/shared/data/todo-data.ts
+++ src/app/shared/data/todo-data.ts
@@ -4,43 +4,31 @@ export const DATA: Todo[] = [
   {
     title: 'todo title 1',
     description: 'what to do',
-    isShown: false,
-    isDone: false,
     deadlineDate: new Date('2021-11-01'),
   },
   {
     title: 'todo title 2',
     description: 'what to do',
-    isShown: false,
-    isDone: false,
     deadlineDate: new Date('2021-11-01'),
   },
   {
     title: 'todo title 3',
     description: 'what to do',
-    isShown: false,
-    isDone: false,
     deadlineDate: new Date('2021-11-01'),
   },
   {
     title: 'todo title 4',
     description: 'what to do',
-    isShown: false,
-    isDone: false,
     deadlineDate: new Date('2021-11-01'),
   },
   {
     title: 'todo title 5',
     description: 'what to do',
-    isShown: false,
-    isDone: false,
     deadlineDate: new Date('2021-11-01'),
   },
   {
     title: 'todo title 6',
     description: 'what to do',
-    isShown: false,
-    isDone: false,
     deadlineDate: new Date('2021-11-01'),
   },
 ];
